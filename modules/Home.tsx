3facffb2ca9e8ce25e3dd8bcda25e18435ae1379
purich.s <purich.s@entronica.co.th>
2021-07-25 01:58:30 +0700

    [Update] rollback to home module

---
+++ modules/Home.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import Products from 'components/products';
+ 
+const Home = ({products}) => {
+    return (
+        <Container found={products.length > 0}>
+            <Content>
+                <Amount>Products ({products.length})</Amount>
+                <Products products={products} />
+            </Content>
+        </Container>
+    );
+}
+
+export default Home;
+
+
+const Container = styled.div`
+    margin-top: 76px;
+
+    @media only screen and (max-width:919px) {
+        margin-top:60px;
+    }
+
+    ${({ found }) => found ? `
+        min-height: 100vh;
+    ` : `
+        height: calc(100vh - 130px);
+
+        @media only screen and (max-width:919px) {
+            height: calc(100vh - 114px);
+        }
+    `}
+`;
+
+const Content = styled.div`
+    max-width: 1206px;
+    width: auto;
+    margin: 0 auto;
+    padding: 50px 20px;
+
+    @media only screen and (max-width:919px) {
+        padding: 36px 20px;
+    }
+`;
+
+const Amount = styled.div`
+    font-size: 16px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.black};
+    padding-bottom: 20px;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import { GetServerSideProps } from 'next';
-import styled from 'styled-components';
-import Products from 'components/products';
+import HomeModule from 'modules/Home';
 import axios from 'axios';
 
 const Home = ({ products }) => {
-  return (
-    <Container found={products.length > 0}>
-      <Content>
-        <Amount>Products ({products.length})</Amount>
-        <Products products={products} />
-      </Content>
-    </Container>
-  )
+  return <HomeModule products={products}/>
 
 };
 
@@ -25,41 +17,5 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 export default Home;
-
-const Container = styled.div`
-    margin-top: 76px;
-
-    @media only screen and (max-width:919px) {
-        margin-top:60px;
-    }
-
-    ${({ found }) => found ? `
-        min-height: 100vh;
-    ` : `
-        height: calc(100vh - 130px);
-
-        @media only screen and (max-width:919px) {
-            height: calc(100vh - 114px);
-        }
-    `}
-`;
-
-const Content = styled.div`
-    max-width: 1206px;
-    width: auto;
-    margin: 0 auto;
-    padding: 50px 20px;
-
-    @media only screen and (max-width:919px) {
-        padding: 36px 20px;
-    }
-`;
-
-const Amount = styled.div`
-    font-size: 16px;
-    font-weight: 600;
-    color: ${({ theme }) => theme.colors.black};
-    padding-bottom: 20px;
-`;
 
 
